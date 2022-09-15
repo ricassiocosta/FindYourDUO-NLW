3acf318afaf9239f541e4f3361ee2e59fe557cab
ricassiocosta <ricassio_costa@outlook.com>
2022-09-14 22:14:19 -0300

    creating routes and setting up sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,28 @@ import express from 'express'
 
 const app = express()
 
-app.get("/ads", (req, res) => {
-    return res.json([
+app.get("/games", (request, response) => {
+    return response.json([])
+})
+
+app.post("/ads", (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get("/games/:id/ads", (request, response) => {
+    //const gameId = request.params.id
+
+    return response.json([
         {"id": 1, "name": "name 1"},
         {"id": 2, "name": "name 2"},
         {"id": 3, "name": "name 3"},
         {"id": 4, "name": "name 4"},
         {"id": 5, "name": "name 5"},
     ])
+})
+
+app.get("/ads/:id/discord", (request, response) => {
+    return response.json([])
 })
 
 app.listen(3333)
